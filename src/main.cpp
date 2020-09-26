8ce894aea5cfda9da4e553ba84f1dba36e9b629c
bcleenders <bcleenders@gmail.com>
2020-09-25 23:38:40 -0400

    Simple WiFi scanner
    Prints something like
    
    ```
    Scan done!
     26 networks found.
    ------------------------------------------------------
    | SSID                    | signal | encryption      |
     ----------------------------------------------------
    |             <wifi name> | -47 dB |        WPA2_PSK |
    |             <wifi name> | -49 dB |        WPA2_PSK |
    |             <wifi name> | -76 dB |    WPA_WPA2_PSK |
    |             <wifi name> | -76 dB |    WPA_WPA2_PSK |
    |             <wifi name> | -79 dB |        WPA2_PSK |
    |             <wifi name> | -81 dB |        WPA2_PSK |
    ------------------------------------------------------
    ```
    
    and blinks light on refresh.

---
+++ src/main.cpp
@@ -0,0 +1,81 @@
+#include <Arduino.h>
+#include <WiFi.h>
+
+int ledPin = 2;
+
+void setup()
+{
+  Serial.begin(115200);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  delay(100);
+
+  Serial.println("Setup done");
+
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
+}
+
+void loop()
+{
+  Serial.println("Starting scan");
+
+  int n = WiFi.scanNetworks();
+  Serial.println("Scan done!");
+
+  char line[64];
+  snprintf(line, sizeof(line), "%3i networks found.", n);
+  Serial.println(line);
+
+  Serial.println("------------------------------------------------------");
+  Serial.println("| SSID                    | signal | encryption      |");
+  Serial.println(" ----------------------------------------------------");
+
+  for (int i = 0; i < n; i++)
+  {
+    String ssid = WiFi.SSID(i);
+    if (ssid.length() > 23)
+    {
+      ssid = ssid.substring(0, 20) + "...";
+    }
+
+    int rssi = WiFi.RSSI(i);
+    String encryption;
+
+    switch (WiFi.encryptionType(i))
+    {
+    case WIFI_AUTH_OPEN:
+      encryption = "open";
+      break;
+    case WIFI_AUTH_WEP:
+      encryption = "WEP";
+      break;
+    case WIFI_AUTH_WPA_PSK:
+      encryption = "WPA_PSK";
+      break;
+    case WIFI_AUTH_WPA2_PSK:
+      encryption = "WPA2_PSK";
+      break;
+    case WIFI_AUTH_WPA_WPA2_PSK:
+      encryption = "WPA_WPA2_PSK";
+      break;
+    case WIFI_AUTH_WPA2_ENTERPRISE:
+      encryption = "WPA2_ENTERPRISE";
+      break;
+    default:
+      encryption = "N/A";
+    }
+
+    // String
+    snprintf(line, sizeof(line), "| %23s | %3i dB | %15s |", ssid.c_str(), rssi, encryption.c_str());
+    Serial.println(line);
+  }
+  Serial.println("------------------------------------------------------");
+  Serial.println("");
+
+  digitalWrite(ledPin, HIGH);
+  delay(250);
+  digitalWrite(ledPin, LOW);
+  delay(4750);
+}
